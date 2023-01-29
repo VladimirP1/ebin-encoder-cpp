1a6d104acc002c68d32349152e6bcec6dec7ac9d
Vladimir Pinchuk <vladimir.pinchuk01@gmail.com>
2023-01-29 21:04:04 +0300

    fix header

--- lib/writer.hpp
+++ lib/writer.hpp
@@ -17,4 +17,8 @@ size_t write_gyro_data(quant::State& state, quat::quat const* quats, size_t n_qu
 size_t write_accel_setup(uint8_t block_size, uint8_t accel_range, uint8_t* out, size_t n_out);
 
 size_t write_accel_data(int16_t const* acc_data, size_t n_acc_data, uint8_t* out, size_t n_out);
+
+size_t write_global_time(int32_t ofs_us, uint8_t* out, size_t n_out);
+
+size_t write_imu_orient(char const* orient, uint8_t* out, size_t n_out);
 }  // namespace writer
